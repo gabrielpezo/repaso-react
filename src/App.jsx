045ee5e47ada5a3e8bf2997302ea4219c0c00443
gabriel pezo <gabrielpezorojas@gmail.com>
2024-06-24 21:59:48 -0400

    estilos añadidos

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,61 @@
 import { useState } from 'react';
-import './App.css'
-import {NavBar} from './components/users/NavBar'
-import Card from './components/users/Card'
+import './App.css';
+import { NavBar } from './components/users/NavBar';
+import Card from './components/users/Card';
 
 function App() {
-  const [ productDetailP,setProductDetail] = useState({})
-  const [ listOfProduct, setListOfProduct ] = useState([
+  const [productDetailP, setProductDetail] = useState({});
+  const [listOfProduct, setListOfProduct] = useState([
     {
-      title: "producto 1",
-      description: "ieudfbvouerigbpae9rubnqar3eubebirvuebv",
+      title: "Producto 1",
+      description: "Descripción del producto 1",
     },
   ]);
 
   const handleChange = (e) => {
     setProductDetail({
       ...productDetailP,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setListOfProduct([...listOfProduct, productDetailP])
-  }
+    e.preventDefault();
+    setListOfProduct([...listOfProduct, productDetailP]);
+  };
 
   return (
     <>
       <NavBar />
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={handleChange}
-          type="text"
-          min="2"
-          max="20"
-          placeholder="Título del producto"
-          name="title"
-        />
-        <input
-          onChange={handleChange}
-          type="text"
-          placeholder="Descripcion del producto"
-          name="description"
-        />
-        <button type="submit" className="btn btn-info">
-          Agregar Producto
-        </button>
-      </form>
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="Título del producto"
+            name="title"
+            className="form-input"
+          />
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="Descripción del producto"
+            name="description"
+            className="form-input"
+          />
+          <button type="submit" className="btn btn-info">
+            Agregar Producto
+          </button>
+        </form>
+      </div>
       <h1>Lista de compras</h1>
-      <div className="d-flex justify-content_between">
-      {
-        listOfProduct.map((product, index) => {
+      <div className="products-container">
+        {listOfProduct.map((product, index) => {
           return <Card key={index} product={product} />;
-        })
-      }
+        })}
       </div>
     </>
   );
 }
 
-export default App
+export default App;
